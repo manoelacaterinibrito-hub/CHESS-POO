975131aaa44269de240597f98defb1be8834edf9
manoelacaterinibrito-hub <manoelacaterinibrito@gmail.com>
2026-06-23 00:03:46 -0300

    apenas peca

--- chess/src/tabuleiro/Peca.java
+++ chess/src/tabuleiro/Peca.java
@@ -1,5 +1,35 @@
 package tabuleiro;
 
 public class Peca {
+    protected Posicao posicao;
+    private Tabuleiro tabuleiro;
 
+    public Peca(Tabuleiro tabuleiro) {
+        // Aqui você considera como se a peça ainda não tivesse sido colocada no tabuleiro.
+        this.tabuleiro = tabuleiro;
+        posicao = null;
+    }
+
+    protected Tabuleiro getTabuleiro() {
+        // Somente classes dentro do mesmo pacote e subclasses poderão executar esse método
+        return tabuleiro;
+    }
+
+    public abstract boolean[][] movimentosPossiveis();
+
+    public boolean movimentoPossivel(Posicao posicao) {
+        return movimentosPossiveis()[posicao.getLinha()][posicao.getColuna()];
+    }
+
+    public boolean qualquerMovimentoPossivel() {
+        boolean[][] mat = movimentosPossiveis();
+        for (int i = 0; i< mat.length; i++) {
+            for (int j = 0; j<mat.length; j++) {
+                if (mat[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
