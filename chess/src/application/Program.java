256dcffce97e51da6ad1fbc22e758bc236d61026
jotape796 <dossantos.estudante@gmail.com>
2026-06-23 07:30:25 -0300

    Ajustando!

---
+++ chess/src/application/Program.java
@@ -0,0 +1,62 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import Xadrez.ExcecaoXad;
+import Xadrez.PartidaXad;
+import Xadrez.PecaXad;
+import Xadrez.Pecas.*;
+import Xadrez.PosicaoXad;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		PartidaXad partida = new PartidaXad();
+		List<PecaXad> capturados = new ArrayList<>();
+		
+		while(!partida.getCheckmate()) {
+			try {
+				Visual.limparTela();
+				Visual.printPartida(partida, capturados);
+				System.out.println();
+				System.out.println("Origem: ");
+				PosiçaoXadrez origem = Visual.lerPosiçaoXadrez(sc);
+				
+				boolean[][] movimentosPossiveis = partida.movimentosPossiveis(origem);
+				Visual.limparTela();
+				Visual.printTabuleiro(partida.verPeças(), movimentosPossiveis);
+				
+				System.out.println();
+				System.out.println("Fim do movimento: ");
+				PosiçaoXadrez fim = Visual.lerPosiçaoXadrez(sc);
+				
+				PeçaXadrez pecaCapturada = partida.fazerMovimentoXadrez(origem, fim);
+				
+				if (pecaCapturada != null) {
+					capturados.add(pecaCapturada);
+				}
+				
+				if (partida.getPromovido() != null) {
+					System.out.print("Digite a letra da peça que você deseja evoluir [B/C/Q/T]: ");
+					String tipo = sc.nextLine().toUpperCase();
+					while (!tipo.equals("B") && !tipo.equals("Q") && !tipo.equals("C") && !tipo.equals("T")) {
+						System.out.print("Valor inválido. Digite a letra da peça que você deseja evoluir [B/C/Q/T]: ");
+						tipo = sc.nextLine().toUpperCase();
+					}
+					partida.substituirPeçaPromovida(tipo);
+				}
+			}
+			catch (ExceçaoXadrez e){
+				System.out.println(e.getMessage());
+				sc.nextLine();			}
+			}
+		Visual.limparTela();
+		Visual.printPartida(partida, capturados);
+		}
+		
+	}
+
